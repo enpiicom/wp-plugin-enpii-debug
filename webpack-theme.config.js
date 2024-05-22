8372e8b891c377a4306c941f21a0e3c0a01df087
Trac Nguyen <npbtrac@yahoo.com>
2024-05-22 13:13:42 +0700

    Add task runner stuffs

---
+++ webpack-theme.config.js
@@ -0,0 +1,11 @@
+/**
+ * WEBPACK CONFIGURATION
+ */
+
+const baseConfig = require('./webpack-base.config');
+
+const themeVariables = require('./webpack-theme.var.config');
+
+module.exports = function (env, argv) {
+    return baseConfig.buildConfig(themeVariables, argv.mode);
+};
